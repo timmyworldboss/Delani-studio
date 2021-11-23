6000b0b5cb873f2f1a9d2bb7f1a2f485bb4271e5
timmyworldboss <timothy.ndung'u@student.moringaschool.com>
2021-11-23 23:28:12 +0300

    js mods

--- index.js
+++ index.js
@@ -1,87 +1,104 @@
-$(document).ready(function () {
-    $("#des,#design").click(function () {
-        $(".hide0").toggle();
-        $("#design").toggle();
+$(document).ready(function(){
+    //this fuction adds smooth scrolling to html
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
     });
-    $("#dev,#development").click(function () {
-        $(".hide1").toggle();
-        $("#development").toggle();
+  });
+  
+  $(document).ready(function() {
+    $('#contactUs form').submit(function() {
+      var nameInput = $('input#name').val();
+      var emailInput = $('input#email').val();
+      var mrssageInput = $('input#message').val();
+      alert('Thank you '+ nameInput + ', we have received your message!')
+    //  document.getElementById("#contactUs").reset();
+      event.preventDefault();
     });
-    $("#prod,#product").click(function () {
-        $(".hide2").toggle();
-        $("#product").toggle();
+  });
+  
+  //what we do click toggles
+  $(document).ready(function() {
+    $('#designIcon').click(function(){
+      $('#designIcon img').toggle();
+      $('#designP').toggle();
     });
-    $("#row1").hover(function () {
-        $(".portfolio1").css({"opacity": "0.2", "transition": "2s"});
-        $("#text1").show();
-
-    }, function () {
-        $(".portfolio1").css("opacity", "1");
-        $("#text1").hide();
+  });
+  
+  $(document).ready(function() {
+    $('#developmentIcon').click(function(){
+      $('#developmentIcon img').toggle();
+      $('#developmentP').toggle();
     });
-    $("#row2").hover(function () {
-        $(".portfolio2").css({"opacity": "0.2", "transition": "2s"});
-        $("#text2").show();
-
-    }, function () {
-        $(".portfolio2").css("opacity", "1");
-        $("#text2").hide();
+  });
+  
+  $(document).ready(function() {
+    $('#productManagementIcon').click(function(){
+      $('#productManagementIcon img').toggle();
+      $('#productManagementP').toggle();
     });
-    $("#row3").hover(function () {
-        $(".portfolio3").css({"opacity": "0.2", "transition": "2s"});
-        $("#text3").show();
-
-    }, function () {
-        $(".portfolio3").css("opacity", "1");
-        $("#text3").hide();
+  });
+  
+  //portfolio hover toggles
+  $(document).ready(function() {
+    $('#blackProject').hover(function(){
+      $('#blackProject').toggle();
+      $('#blackProjectH2').toggle();
     });
-    $("#row4").hover(function () {
-        $(".portfolio4").css({"opacity": "0.2", "transition": "2s"});
-        $("#text4").show();
-
-    }, function () {
-        $(".portfolio4").css("opacity", "1");
-        $("#text4").hide();
+  });
+  
+  $(document).ready(function() {
+    $('#haveAGoodDayProject').hover(function(){
+      $('#haveAGoodDayProject').toggle();
+      $('#haveAGoodDayProjectH2').toggle();
     });
-    $("#row5").hover(function () {
-        $(".portfolio5").css({"opacity": "0.2", "transition": "2s"});
-        $("#text5").show();
-
-    }, function () {
-        $(".portfolio5").css("opacity", "1");
-        $("#text5").hide();
+  });
+  
+  $(document).ready(function() {
+    $('#mightyOntarioProject').hover(function(){
+      $('#mightyOntarioProject').toggle();
+      $('#mightyOntarioProjectH2').toggle();
     });
-    $("#row6").hover(function () {
-        $(".portfolio6").css({"opacity": "0.2", "transition": "2s"});
-        $("#text6").show();
-
-    }, function () {
-        $(".portfolio6").css("opacity", "1");
-        $("#text6").hide();
+  });
+  
+  $(document).ready(function() {
+    $('#buttonStylesProject').hover(function(){
+      $('#buttonStylesProject').toggle();
+      $('#buttonStylesProjectH2').toggle();
     });
-    $("#row7").hover(function () {
-        $(".portfolio7").css({"opacity": "0.2", "transition": "2s"});
-        $("#text7").show();
-
-    }, function () {
-        $(".portfolio7").css("opacity", "1");
-        $("#text7").hide();
+  });
+  
+  $(document).ready(function() {
+    $('#materialImageProject').hover(function(){
+      $('#materialImageProject').toggle();
+      $('#materialImageProjectH2').toggle();
     });
-    $("#row8").hover(function () {
-        $(".portfolio8").css({"opacity": "0.2", "transition": "s"});
-        $("#text8").show();
-
-    }, function () {
-        $(".portfolio8").css("opacity", "1");
-        $("#text8").hide();
+  });
+  
+  $(document).ready(function() {
+    $('#burnedLogoProject').hover(function(){
+      $('#burnedLogoProject').toggle();
+      $('#burnedLogoProjectH2').toggle();
     });
-
-});
-var submission = function () {
-    var email = document.getElementById("email")
-    if (email == "") {
-        alert("")
-    } else {
-        alert(' Your message has been received. Thank you for reaching out to us.')
-    }
-}
+  });
+  
+  $(document).ready(function() {
+    $('#jimCarreyProject').hover(function(){
+      $('#jimCarreyProject').toggle();
+      $('#jimCarreyProjectH2').toggle();
+    });
+  });
+  
+  $(document).ready(function() {
+    $('#baileyWonderProject').hover(function(){
+      $('#baileyWonderProject').toggle();
+      $('#baileyWonderProjectH2').toggle();
+    });
+  });
