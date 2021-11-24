c6020e20eb60afe3730a168c87bc0af68e58af61
timmyworldboss <timothy.ndung'u@student.moringaschool.com>
2021-11-24 08:30:24 +0300

    j script

--- index.js
+++ index.js
@@ -1,104 +1,64 @@
-$(document).ready(function(){
-    //this fuction adds smooth scrolling to html
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-          window.location.hash = hash;
-        });
-      }
-    });
-  });
+$(document).ready(function() {
+    $("#contact form").submit(function(event) {
+      var fullName = $("input#name").val();
+      var email = $("input#mail").val();
+      var text = $("textarea#textarea").val();
   
-  $(document).ready(function() {
-    $('#contactUs form').submit(function() {
-      var nameInput = $('input#name').val();
-      var emailInput = $('input#email').val();
-      var mrssageInput = $('input#message').val();
-      alert('Thank you '+ nameInput + ', we have received your message!')
-    //  document.getElementById("#contactUs").reset();
+      $(".name").text(fullName);
+      $(".mail").text(email);
+      $(".text").text(text);
+  
+      if (fullName == "" || email == "") {
+        alert("Please fill in all your details");
+      } else if (text == "") {
+        alert("Kindly write a message");
+      } else {
+        alert(
+          fullName +
+            " we have received your message. Thank you for reaching out to us."
+        );
+      }
+      
       event.preventDefault();
     });
   });
   
-  //what we do click toggles
-  $(document).ready(function() {
-    $('#designIcon').click(function(){
-      $('#designIcon img').toggle();
-      $('#designP').toggle();
-    });
+  $("#design").click(function() {
+    $("#design p").toggle();
+    $("#design img").toggle();
   });
   
-  $(document).ready(function() {
-    $('#developmentIcon').click(function(){
-      $('#developmentIcon img').toggle();
-      $('#developmentP').toggle();
-    });
+  $("#dev").click(function() {
+    $("#dev p").toggle();
+    $("#dev img").toggle();
   });
   
-  $(document).ready(function() {
-    $('#productManagementIcon').click(function(){
-      $('#productManagementIcon img').toggle();
-      $('#productManagementP').toggle();
-    });
+  $("#product").click(function() {
+    $("#product p").toggle();
+    $("#product img").toggle();
   });
   
-  //portfolio hover toggles
-  $(document).ready(function() {
-    $('#blackProject').hover(function(){
-      $('#blackProject').toggle();
-      $('#blackProjectH2').toggle();
-    });
+  $(".col-md-3 #w1").hover(function() {
+    $("#c1").toggle();
   });
-  
-  $(document).ready(function() {
-    $('#haveAGoodDayProject').hover(function(){
-      $('#haveAGoodDayProject').toggle();
-      $('#haveAGoodDayProjectH2').toggle();
-    });
+  $(".col-md-3 #w2").hover(function() {
+    $("#c2").toggle();
   });
-  
-  $(document).ready(function() {
-    $('#mightyOntarioProject').hover(function(){
-      $('#mightyOntarioProject').toggle();
-      $('#mightyOntarioProjectH2').toggle();
-    });
+  $(".col-md-3 #w3").hover(function() {
+    $("#c3").toggle();
   });
-  
-  $(document).ready(function() {
-    $('#buttonStylesProject').hover(function(){
-      $('#buttonStylesProject').toggle();
-      $('#buttonStylesProjectH2').toggle();
-    });
+  $(".col-md-3 #w4").hover(function() {
+    $("#c4").toggle();
   });
-  
-  $(document).ready(function() {
-    $('#materialImageProject').hover(function(){
-      $('#materialImageProject').toggle();
-      $('#materialImageProjectH2').toggle();
-    });
+  $(".col-md-3 #w5").hover(function() {
+    $("#c5").toggle();
   });
-  
-  $(document).ready(function() {
-    $('#burnedLogoProject').hover(function(){
-      $('#burnedLogoProject').toggle();
-      $('#burnedLogoProjectH2').toggle();
-    });
+  $(".col-md-3 #w6").hover(function() {
+    $("#c6").toggle();
   });
-  
-  $(document).ready(function() {
-    $('#jimCarreyProject').hover(function(){
-      $('#jimCarreyProject').toggle();
-      $('#jimCarreyProjectH2').toggle();
-    });
+  $(".col-md-3 #w7").hover(function() {
+    $("#c7").toggle();
   });
-  
-  $(document).ready(function() {
-    $('#baileyWonderProject').hover(function(){
-      $('#baileyWonderProject').toggle();
-      $('#baileyWonderProjectH2').toggle();
-    });
+  $(".col-md-3 #w8").hover(function() {
+    $("#c8").toggle();
   });
